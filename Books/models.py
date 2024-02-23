6e9979e083f5f8080576178f8bbb74edfc57b65f
Michael322312 <zomisha0808@gmail.com>
2024-02-23 12:34:45 +0200

    Created model Library book with clomuns and class meta

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class LibraryBook(models.Model):
+    title = models.CharField(max_length=63)
+    author = models.CharField(max_length=63)
+    isbn = models.CharField(max_length=63, unique=True)
+    available = models.BooleanField()
+
+    class Meta:
+        db_table = "library_books"
+        ordering = ["author"]
